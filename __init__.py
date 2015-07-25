206e7a67e8bb6baf14257a9afbdfca0c6b8e78a6
phamhm <phamhm@gmail.com>
2015-07-24 18:21:57 -0700

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,10 @@
+from . import (
+    motion,
+)
+
+from .motion import LabelObject
+from .motion import BufferUndoCommand
+from .motion import AddLabelsCommand
+from .motion import JumpToLabelCommand
+from .motion import draw_labels
+from .motion import label_generator_singledouble
